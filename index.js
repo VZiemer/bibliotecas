64fe2398e60cda29cb1a0526bf08a9be9c9af893
VZiemer <vanius@live.com>
2018-10-03 12:21:24 -0300

    geral

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    Endereco: require('./lib/endereco'),
+    Pessoa: require('./lib/pessoa'),
+    Dinheiro: require('./lib/dinheiro'),
+    Venda: require('./lib/venda'),
+    Produto: require('./lib/produto')
+};
